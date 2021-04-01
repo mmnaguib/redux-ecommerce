2a8bc6c6999e086e2c1f9908423aef4859436409
mohamed <mnaguib126@gmail.com>
2021-04-01 10:26:47 +0200

    add home page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
+import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
     <App />
-  </React.StrictMode>,
+  </PersistGate>
+</Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,17 @@
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import productsReducer from './products/products_reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['sidebar'],
+};
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
+
+export default persistReducer(persistConfig, rootReducer);
